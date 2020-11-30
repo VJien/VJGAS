85c5f42aebc895477def1974da8718063b36bfb8
Jien <389278697@qq.com>
2020-11-30 17:22:13 +0800

    init

---
+++ Source/VGAS/VGASGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VGASGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VGAS_API AVGASGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
